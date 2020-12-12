e346ebacec4884219966882911b7f081cf5e70db
ryxwaer <ryxwaer@gmail.com>
2020-12-12 20:15:45 +0100

    a

--- server/game.js
+++ server/game.js
@@ -45,7 +45,7 @@ function createGameState() {
       ],
     }],
     food: {},
-    obticle: [],
+    obticle: [{}],
     gridsize: GRID_SIZE,
   };
 }
@@ -168,7 +168,7 @@ function randomFood(state) {
 
 // nahodne generovanie prekazok
 function randomObsticle(state) {
-  while(state.obsticle.length <= OBSTICLES){
+  while($(state.obsticle).length <= OBSTICLES){
     obsticle = {
         x: Math.floor(Math.random() * GRID_SIZE),
         y: Math.floor(Math.random() * GRID_SIZE),
